24a975abe7a0d6fc287247808f49c9000b8c90ef
bbskeelz <schao@gatech.edu>
2016-03-23 22:53:59 -0400

    multiple videos

---
+++ src/cake/InFlightEntertainment2.java
@@ -0,0 +1,32 @@
+public class InFlightEntertainment2{
+	
+	public static boolean f(int[] in, int duration){
+		return f(in, 0, duration);
+	}
+	
+	public static boolean f(int[] in, int index, int duration){
+		for (int i =index ; i < in.length; i++){
+			if (in[i] == duration) return true;
+			if (in[i] > duration) continue;
+			if (in[i] < duration){
+				if (f(in, i+1, duration - in[i]))
+					return true;
+				continue;
+			}
+		}
+		return false;
+	}
+	
+	public static void main(String[] args){
+		
+		int[] in = new int[]{3,2,1,4,5};
+		System.out.println(f(in, 3));  //line 9
+		System.out.println(f(in, 6));  //true
+		in = new int[]{7,2,1};
+		System.out.println(f(in, 10)); //true
+ 		System.out.println(f(in, 4));  //10
+		in = new int[]{2};
+ 		System.out.println(f(in, 4));  //10
+		
+	}
+}
